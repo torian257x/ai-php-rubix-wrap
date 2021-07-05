486aaefdda5ad8665a6fadfbc90e67feb53f808e
jossnaz <lukas.meier@gmail.com>
2021-07-04 23:39:30 -0500

    config.php added to git

---
+++ src/config.php
@@ -0,0 +1,8 @@
+<?php
+
+
+return [
+    'csv_path_output' => __DIR__ . '/../data/csv/',
+    'csv_path_input' => __DIR__ . '/../data/csv/',
+    'ai_model_path_output' => __DIR__ . '/../data/ai_model/',
+];
