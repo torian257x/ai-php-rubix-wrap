0ba8479045afa173bfb8c09c3cf8185776d11d8b
jossnaz <lukas.meier@gmail.com>
2021-08-10 23:06:55 -0500

    functions.php making it possible to load custom config via global constant

--- src/functions.php
+++ src/functions.php
@@ -3,7 +3,12 @@
 if (!function_exists('rubixai_getconfig')) {
   function rubixai_getconfig(string $config_entry = null)
   {
-    $config = require(__DIR__ . '/rubwrap_config.php');
+
+    if(defined('RUBIXAI_CUSTOM_CONFIG')){
+      $config = RUBIXAI_CUSTOM_CONFIG;
+    }else{
+      $config = require(__DIR__ . '/rubwrap_config.php');
+    }
 
     if ($config_entry) {
       return $config[$config_entry] ?? null;
